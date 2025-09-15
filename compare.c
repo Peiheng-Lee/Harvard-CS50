33eb0decae36a660d988a1b910dc7474462ada18
Peiheng-Lee <peihengli70@gmail.com>
2025-09-15 12:21:29 +0000

    Mon, Sep 15, 2025, 8:21 PM +08:00

---
+++ compare.c
@@ -0,0 +1,12 @@
+#include<cs50.h>
+#include<stdio.h>
+int main(void)
+{
+    int x = get_int("What is x? ");
+    int y = get_int("What is y? ");
+
+    if (x<y)
+    {
+        printf("x is less than y\n");
+    }
+}

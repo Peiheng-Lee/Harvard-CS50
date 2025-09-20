d7d9fabe2bb73a5f378d3c3d89dd73139ddfaed4
Peiheng-Lee <peihengli70@gmail.com>
2025-09-20 08:44:17 +0000

    Sat, Sep 20, 2025, 4:44 PM +08:00

---
+++ credit/credit.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+int main(void)
+{
+    long number;
+    do
+    {
+        number = get_long("Number:");
+    }
+    while (number < 1);
+
+}
+
+int luhn_isvalid(long n)
+{
+    1
+}

5ba48a5dd7a7fa68a99ec93c01da31a61286d6b7
Peiheng-Lee <peihengli70@gmail.com>
2025-09-28 12:57:17 +0000

    Sun, Sep 28, 2025, 8:57 PM +08:00

--- Arrays/status.c
+++ Arrays/status.c
@@ -2,4 +2,15 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void)
+int main(int argc; string argv[])
+{
+    if (agrc != 2)
+    {
+        printf("Missing command-line arguments.\n");
+        return 1;
+    }
+    else
+    {
+        printf("0\n");
+    }
+}

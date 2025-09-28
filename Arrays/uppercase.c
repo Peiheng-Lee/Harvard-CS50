624de0c1c09952bce0e2846934e6ecc7cba282ee
Peiheng-Lee <peihengli70@gmail.com>
2025-09-28 12:17:02 +0000

    Sun, Sep 28, 2025, 8:17 PM +08:00

---
+++ Arrays/uppercase.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+int main(void)
+{
+    string name = get_string("Before:");
+    printf("After: ");
+    for (int i = 0, n = strlen(name); i < n; i++)
+    {
+        if (name[i] >= 'a' && name[i] <= 'z')
+        {
+            printf("%c", name[i] - 32);
+        }
+    }
+    printf("\n");
+}

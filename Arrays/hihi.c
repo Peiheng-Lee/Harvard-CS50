32a640089cd7442815930143d2e8e78c35c5bd51
Peiheng-Lee <peihengli70@gmail.com>
2025-09-28 11:49:23 +0000

    Sun, Sep 28, 2025, 7:49 PM +08:00

--- Arrays/hihi.c
+++ Arrays/hihi.c
@@ -6,6 +6,7 @@ int main(void)
     string words[2];
     words[0] = "HI!";
     words[1] = "BYE!";
-    printf("%s\n", words[0]);
-    printf("%s\n", words[1]);
+    printf("%c%c%c\n", words[0][0], words[0][1], words[0][2]);
+    printf("%c%c%c%c\n", words[1][0], words[1][1], words[1][2], words[1][3]);
+    // srings belong to arrays
 }

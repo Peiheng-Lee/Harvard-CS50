53c20f6beb3ff2b47a93de7a4906f410f8a6fb85
Peiheng-Lee <peihengli70@gmail.com>
2025-10-09 08:52:51 +0000

    Thu, Oct 9, 2025, 4:52 PM +08:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -5,14 +5,24 @@ int main(void)
 {
     string word1 = get_char("Player1:");
     string word2 = get_char("Player2:");
-    printf("%s\n", scrabble(word1, word2))
 
+    // Compute the score of each word
+    int score1 = compute_score(word1);
+    int score2 = compute_score(word2);
+
+    printf("%s\n", scrabble(word1, word2));
+
+}
+
+int compute_score(int x)
+{
+    f
 }
 
 string scrabble(string a, string b)
 {
     while (a[i] != "/0")
     {
-        sum1 += 
+        sum1 +=
     }
 }

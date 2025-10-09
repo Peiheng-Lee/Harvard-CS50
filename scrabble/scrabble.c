02847fed58711e289af60d8e24483bcd7a688a32
Peiheng-Lee <peihengli70@gmail.com>
2025-10-09 08:57:38 +0000

    Thu, Oct 9, 2025, 4:57 PM +08:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -1,6 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
+// Points assigned to each letter of the alphabet
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3,
+                1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10}
+
+// prototype
+int compute_score(string word);
+
 int main(void)
 {
     string word1 = get_char("Player1:");
@@ -10,13 +17,15 @@ int main(void)
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
 
-    printf("%s\n", scrabble(word1, word2));
 
 }
 
-int compute_score(int x)
+int compute_score(string word)
 {
-    f
+    for (int i = 0, n = strlen(word); i < n; i++)
+    {
+        sum += 
+    }
 }
 
 string scrabble(string a, string b)

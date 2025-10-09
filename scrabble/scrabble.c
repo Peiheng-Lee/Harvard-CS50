ef5cc1a0bf3ad9c29df301a5f2ed0e52c8b6c5c1
Peiheng-Lee <peihengli70@gmail.com>
2025-10-09 09:33:33 +0000

    Thu, Oct 9, 2025, 5:33 PM +08:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -20,15 +20,15 @@ int main(void)
 
     if (score1 > score2)
     {
-        printf("Player 1 wins!");
+        printf("Player 1 wins!\n");
     }
     else if (score1 < score2)
     {
-        printf("Player 2 wins!");
+        printf("Player 2 wins!\n");
     }
     else
     {
-        printf("Tie!");
+        printf("Tie!\n");
     }
 }
 

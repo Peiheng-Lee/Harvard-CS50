31c765c25107242d68fb35f3bb18c0ce8faf831f
Peiheng-Lee <peihengli70@gmail.com>
2025-10-10 10:39:14 +0000

    Fri, Oct 10, 2025, 6:39 PM +08:00

--- readability/readability.c
+++ readability/readability.c
@@ -4,16 +4,29 @@
 #include <stdio.h>
 #include <string.h>
 
+int count_letters(string text);
+
 int main(void)
 {
     string text = get_string("Text:");
     count_letters()
+    count_words()
 }
 
 int count_letters(string text)
 {
+    count = 0;
     for (i = 0, len = strlen(text); i < len; i++)
     {
-        if text[i] != 
+        if (text[i] != ' ')
+        {
+            count += 1;
+        }
     }
+    return count;
+}
+
+int count_words(string text)
+{
+    p
 }

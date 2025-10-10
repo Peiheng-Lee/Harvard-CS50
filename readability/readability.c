a4953bcec25ec14fbe4b636d82c00aee5cd3f6ff
Peiheng-Lee <peihengli70@gmail.com>
2025-10-10 11:03:50 +0000

    Fri, Oct 10, 2025, 7:03 PM +08:00

--- readability/readability.c
+++ readability/readability.c
@@ -17,7 +17,21 @@ int main(void)
 
     float L = (letters / words) * 100;
     float S = (sentences / words) * 100;
-    float index = 0.0588 * L - 0.296 * S - 15.8
+    float index = 0.0588 * L - 0.296 * S - 15.8;
+
+    int index = round(index);
+    if (index >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else
+    {
+        printf("Grade %i\n", index);
+    }
 }
 
 int count_letters(string text)

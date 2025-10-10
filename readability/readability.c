eba1429ae4881195397f7d02ff075b0000708abb
Peiheng-Lee <peihengli70@gmail.com>
2025-10-10 10:52:18 +0000

    Fri, Oct 10, 2025, 6:52 PM +08:00

--- readability/readability.c
+++ readability/readability.c
@@ -5,12 +5,15 @@
 #include <string.h>
 
 int count_letters(string text);
+int count_words(string text);
+int count_sentences(string text);
 
 int main(void)
 {
     string text = get_string("Text:");
     int letters = count_letters(text);
-    count_words()
+    int words = count_words(text);
+    int sentences = count_sentences(text);
 }
 
 int count_letters(string text)
@@ -30,5 +33,24 @@ int count_words(string text)
 {
     count = 0;
     for (i = 0, len = strlen(text); i < len; i++)
-    if not(isalpha(text[i]))
+    {
+        if (!isalpha(text[i])) // !对逻辑表达式取反
+        {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+int count_sentences(string text)
+{
+    count = 0;
+    for (i = 0, len = strlen(text); i < len; i++)
+    {
+        if (text[i] = '.' || text[i] = '?' || text[i] = '!')
+        {
+            count += 1;
+        }
+    }
+    return count;
 }

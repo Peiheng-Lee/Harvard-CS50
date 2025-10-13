964861234fac20c03829522f1d37f19153f8089a
Peiheng-Lee <peihengli70@gmail.com>
2025-10-13 13:42:39 +0000

    Mon, Oct 13, 2025, 9:42 PM +08:00

---
+++ Algorithms/search1.c
@@ -0,0 +1,10 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    string strings[] = {'battleship', 'boot', 'canon', ', 100, 1, 50};
+
+    int n = get_int("Number:");
+    for (int i = 0; i < 7; i++)
+}

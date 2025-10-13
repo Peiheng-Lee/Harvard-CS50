34a98deb9add1d30d5a107a9e275daeccffa9e60
Peiheng-Lee <peihengli70@gmail.com>
2025-10-13 13:35:42 +0000

    Mon, Oct 13, 2025, 9:35 PM +08:00

--- Algorithms/search.c
+++ Algorithms/search.c
@@ -3,12 +3,12 @@
 
 int main(void)
 {
-    int number[] = {, , , , , , , };
+    int numbers[] = {20, 500, 10, 5, 100, 1, 50};
 
     int n = get_int("Number:");
     for (int i = 0; i < 7; i++)
     {
-        if (number[i] == n)
+        if (numbers[i] == n)
         {
             printf("Found!\n");
         }
